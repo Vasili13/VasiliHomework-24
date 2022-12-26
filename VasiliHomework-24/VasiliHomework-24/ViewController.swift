ddb0f335b0b6057e59d0f5e848cdc91cd95729ac
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2022-12-26 22:30:35 +0300

    Project created

---
+++ VasiliHomework-24/VasiliHomework-24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VasiliHomework-24
+//
+//  Created by Василий Вырвич on 26.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
